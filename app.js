e49203ea6c0a75a05490c5ee34981fa352c74b7b
Rafael Macedo <rafaelmacedosilva88@hotmail.com>
2023-03-11 23:17:28 -0300

    Configuração de porta

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const usuarios = require('./Usuarios');
 const path = require('path');
 
