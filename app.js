4afaf6dc08fab7418cf80a00b1230843ad7b86d0
Rafael Macedo <rafaelmacedosilva88@hotmail.com>
2023-03-13 23:30:19 -0300

    Mudança de texto

--- app.js
+++ app.js
@@ -5,57 +5,21 @@ const PORT = process.env.PORT || 5000;
 const path = require('path');
 const usuarioRouter = require('./routes/usuarios.routes');
 
+// MIDDLEWARES
 app.set('view engine','ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// CONNECTION
 mongoose.connect("mongodb+srv://rafaelsilva2:bmabjJFMFOuRgNcD@treinamento.usistsp.mongodb.net/?retryWrites=true&w=majority").then((response) => {
     console.log('conectado!')
 });
 
+// ROUTES
 app.use(usuarioRouter);
 
-
-// app.get('/usuarios', (req,res,next) => {
-//     res.render('forms');
-// })
-
-// app.post('/usuarios', async (req,res,next) => {
-//     const nome = req.body.nome;
-//     const email = req.body.email;
-//     const telefone = req.body.telefone;
-//     const senha = req.body.senha;
-
-//     await usuarios.create({
-//         nome,
-//         email,
-//         telefone,
-//         senha
-//     }).then((response) => {
-//         res.redirect('/');
-//     }).catch(error => {
-//         res.redirect('/error')
-//     })
-// })
-
-// app.get('/', async (req,res,next) => {
-//     const users = await usuarios.find({});
-//     res.render('list', { users: users })
-// })
-
-// // Exclui um elemento pelo ID
-// app.get('/contato/:id', async (req,res,next) => {
-//     const id = req.params.id;
-//     if(id){
-//         await usuarios.deleteOne({_id: id}).then(response => {
-//             res.redirect('/');
-//         })
-//     }
-// })
-
-
+// LISTEN 
 app.listen(PORT, () => {
-    console.log('Servidor executando na port:3000');
+    console.log('Servidor executando na port:', PORT);
 })
-
